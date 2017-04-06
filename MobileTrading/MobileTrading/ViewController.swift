80d7c219929fa7b9863a40a1b5e9ca56cabda235
minhndcs52 <minh.nguyen@siliconprime.us>
2017-04-06 11:00:09 +0700

    Init core project

---
+++ MobileTrading/MobileTrading/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileTrading
+//
+//  Created by Nguyen Minh on 4/6/17.
+//  Copyright © 2017 AHDEnglish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
