c8afcde028b4fb84404e9d5ab8e5a617e544d6d4
minhndcs52 <nguyenminhkhmt@gmail.com>
2017-04-08 08:37:59 +0700

    Cryptor & test code.

---
+++ ModulesTests/RNCryptorTests/Data.swift
@@ -0,0 +1,44 @@
+//
+//  ByteArray.swift
+//
+//  Copyright © 2015 Rob Napier. All rights reserved.
+//
+//  This code is licensed under the MIT License:
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+
+import Foundation
+
+internal extension String {
+    var dataFromHexEncoding: Data? {
+        let strip = [Character]([" ", "<", ">", "\n", "\t"])
+        let input = characters.filter { c in !strip.contains(c)}
+
+        guard input.count % 2 == 0 else { return nil }
+
+        let data = NSMutableData()
+        for i in stride(from: 0, to: input.count, by: 2) {
+            guard var value = UInt8(String(input[i...i+1]), radix: 16) else { return nil }
+            data.append(&value, length:1)
+        }
+
+        return data as Data
+    }
+}
